a7eb5923977b24286288525f9b90f701cd5ec67f
MMadsen99 <math9082@easj.edu.dk>
2020-11-04 19:31:12 +0100

    test om commit viser sig på kalenderen 2.0

--- src/commit.java
+++ src/commit.java
@@ -2,6 +2,8 @@ public class commit {
     //blablabla
 
     public static void main(String[] args) {
+        //Den her linje printer balbalbla
         System.out.println("blablabla");
+
     }
 }
